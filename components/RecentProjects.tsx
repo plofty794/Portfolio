b0650a69849732670c1f0ad1a2bc93ab5c139dbd
Ace G <aceguevarra48@gmail.com>
2024-06-21 19:38:30 +0800

    first commit

---
+++ components/RecentProjects.tsx
@@ -0,0 +1,87 @@
+/* eslint-disable @next/next/no-img-element */
+"use client";
+
+import { ExternalLinkIcon } from "lucide-react";
+
+import { projects } from "@/data";
+import { PinContainer } from "./ui/Pin";
+
+const RecentProjects = () => {
+  return (
+    <section id="projects">
+      <div className="py-20">
+        <h1 className="heading">
+          A collection of <span className="text-purple">recent projects</span>
+        </h1>
+        <div className="flex flex-wrap items-center justify-center p-4 gap-x-24 gap-y-8 mt-10">
+          {projects.map((item) => (
+            <div
+              className="sm:h-[41rem] h-[32rem] flex items-center justify-center sm:w-[570px] w-[80vw]"
+              key={item.id}
+            >
+              <PinContainer
+                title="Visit"
+                href="https://post-it-ecru.vercel.app/"
+              >
+                <a href={item.link} target={"_blank"}>
+                  <div className="relative flex items-center justify-center w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10">
+                    <div
+                      className="relative w-full h-full overflow-hidden rounded-3xl"
+                      style={{ backgroundColor: "#13162D" }}
+                    >
+                      <img src="/bg.png" alt="bgimg" />
+                    </div>
+                    <img
+                      src={item.img}
+                      alt="cover"
+                      className="z-[5] absolute w-3/4 mx-auto object-cover"
+                    />
+                  </div>
+
+                  <h1 className="font-bold lg:text-2xl md:text-xl text-base line-clamp-1">
+                    {item.title}
+                  </h1>
+
+                  <p
+                    className="lg:text-xl lg:font-normal font-light text-sm line-clamp-2"
+                    style={{
+                      color: "#BEC1DD",
+                      margin: "1vh 0",
+                    }}
+                  >
+                    {item.des}
+                  </p>
+
+                  <div className="flex items-center justify-between mt-7 mb-3">
+                    <div className="flex items-center">
+                      {item.iconLists.map((icon, index) => (
+                        <div
+                          key={index}
+                          className="border border-white/[.2] rounded-full black-gradient lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center"
+                          style={{
+                            transform: `translateX(-${5 * index + 2}px)`,
+                          }}
+                        >
+                          <img src={icon} alt="icon5" className="p-2" />
+                        </div>
+                      ))}
+                    </div>
+
+                    <div className="flex justify-center items-center">
+                      <p className="flex lg:text-xl md:text-xs text-sm text-purple">
+                        Check Live Site
+                      </p>
+                      <ExternalLinkIcon className="ms-3" color="#CBACF9" />
+                    </div>
+                  </div>
+                </a>
+              </PinContainer>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default RecentProjects;
---
+++ data/index.ts
@@ -0,0 +1,216 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title:
+      "Currently building a Social News Aggregation and Content Rating System",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Post It - Social News Aggregation and Content Rating System",
+    des: "Dive into the diverse world of online communities with Post It, a platform where you can explore and discuss countless topics, share ideas, and connect with like-minded individuals.",
+    img: "/Post it.png",
+    iconLists: [
+      "/re.svg",
+      "/tail.svg",
+      "/ts.svg",
+      "/react-query-seeklogo.svg",
+      "/shadcn-ui-seeklogo.svg",
+    ],
+    link: "https://post-it-ecru.vercel.app/",
+  },
+  // {
+  //   id: 2,
+  //   title: "Yoom - Video Conferencing App",
+  //   des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
+  //   img: "/p2.svg",
+  //   iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+  //   link: "/ui.yoom.com",
+  // },
+  // {
+  //   id: 3,
+  //   title: "AI Image SaaS - Canva Application",
+  //   des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
+  //   img: "/p3.svg",
+  //   iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+  //   link: "/ui.aiimg.com",
+  // },
+  // {
+  //   id: 4,
+  //   title: "Animated Apple Iphone 3D Website",
+  //   des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+  //   img: "/p4.svg",
+  //   iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+  //   link: "/ui.apple.com",
+  // },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "I had an exceptional experience using the Post It application developed by Ace. The interface is intuitive, clean, and user-friendly, making it incredibly easy to navigate and use. Ace's attention to detail and dedication to creating a seamless user experience is evident in every aspect of the application. Highly recommend Ace for any development projects!",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "The Post It website offers a sleek and minimalist design that enhances usability. The functionality is straightforward and efficient, ensuring users can quickly post and manage notes. However, adding more customization options for note appearances and incorporating user account features for personalized experiences would be great enhancements",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "I had an exceptional experience using the Post It application developed by Ace. The interface is intuitive, clean, and user-friendly, making it incredibly easy to navigate and use. Ace's attention to detail and dedication to creating a seamless user experience is evident in every aspect of the application. Highly recommend Ace for any development projects!",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "The Post It website offers a sleek and minimalist design that enhances usability. The functionality is straightforward and efficient, ensuring users can quickly post and manage notes. However, adding more customization options for note appearances and incorporating user account features for personalized experiences would be great enhancements",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "nodejs",
+    img: "/nodejs-icon.svg",
+    nameImg: "/nodejs-horizontal.svg",
+  },
+  {
+    id: 3,
+    name: "mongoDB",
+    img: "/mongodb.svg",
+    nameImg: "/mongodb.svg",
+  },
+  {
+    id: 4,
+    name: "expressJS",
+    img: "/s.svg",
+    nameImg: "/expressjs.svg",
+  },
+  {
+    id: 5,
+    name: "firebase",
+    img: "/firebase.svg",
+    nameImg: "/firebase.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Frontend Developer Intern",
+    desc: "Assisted in the development of a web-based platform using Laravel's Blade components and Bladewind UI, enhancing interactivity.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Lead Full Stack Developer",
+    desc: "Designed and developed a web app for our capstone project using MERN stack, Firebase auth, and WebSockets.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  // {
+  //   id: 3,
+  //   title: "Freelance App Dev Project",
+  //   desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+  //   className: "md:col-span-2", // change to md:col-span-2
+  //   thumbnail: "/exp3.svg",
+  // },
+  // {
+  //   id: 4,
+  //   title: "Lead Frontend Developer",
+  //   desc: "Developed and maintained user-facing features using modern frontend technologies.",
+  //   className: "md:col-span-2",
+  //   thumbnail: "/exp4.svg",
+  // },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    url: "https://github.com/plofty794",
+  },
+  {
+    id: 2,
+    img: "/link.svg",
+    url: "https://www.linkedin.com/in/ace-guevarra-11a08b27b/",
+  },
+];
